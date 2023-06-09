d305369b5027748d6490c902e945127e34556b81
Wathsala Bandara <60806985+WathsalaBandara@users.noreply.github.com>
2023-06-09 12:16:53 +0530

    Add files via upload

---
+++ tutorialsms.java
@@ -0,0 +1,10 @@
+import com.teknikindustries.bulksms.SMS;
+
+
+public class tutorialsms {
+    public static void main(String[] args) {
+        SMS smstute=new SMS();
+        smstute.SendSMS("wathsala_98", "Wathsala_98", "Hello", "0715191223", "http://bulksms.2way.co.za/eapi/submission/send_sms/2/2.0");
+    }
+    
+}
